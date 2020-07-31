3f7f0b523ae5638e17bf8c0f99c9e0ec1d44a691
TheSavageTeddy <TheSavageTeddy@users.noreply.github.com>
2020-07-31 20:10:47 +0800

    added more constanent sounds

--- randomStringGenerator.py
+++ randomStringGenerator.py
@@ -1,5 +1,9 @@
-import random
+#this is a tool to generate either completely random strings with alphabetical characters in them, or
+#for people who cant think of a name for something and want inspiration! (me)
+#                           - TheSavageTeddy
 
+
+import random
 
 length=int(input("length of string: "))
 option=input('''
@@ -8,16 +12,19 @@ option=input('''
 ''')
 
 amount=int(input("amount to generate: "))
-
+#consenents
 c=['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
+#vowels
 v=['a','e','i','o','u']
-all=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-cs=['ss','tt','ll','gh','th','st','gt','pt','ns']
+#alphabet
+alphabet=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+#consenent sounds
+cs=['p','b','t','d','c','ck','g','f','v','th','s','z','sh','ch','ng','ll']
 if option == '1':
     for i in range(0, amount):
         string=""
         for i in range(0, length):
-            string=string+random.choice(all)
+            string=string+random.choice(alphabet)
         print(string)
 elif option == '2':
     for i in range(0, amount):
