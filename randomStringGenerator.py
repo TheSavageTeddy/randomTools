aa8ac758ff918acbfd13ceacde4257405cce992d
TheSavageTeddy <TheSavageTeddy@users.noreply.github.com>
2020-08-03 12:31:51 +0800

    added range of length of string

--- randomStringGenerator.py
+++ randomStringGenerator.py
@@ -5,7 +5,12 @@
 
 import random
 
-length=int(input("length of string: "))
+length=input(
+'''
+range of length of string: 
+(5 7 for 5 to 7)
+''')
+length=length.split()
 option=input('''
 (1) completely random
 (2) readable/sayable words
@@ -19,24 +24,24 @@ v=['a','e','i','o','u']
 #alphabet
 alphabet=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 #consenent sounds
-cs=['p','b','t','d','c','ck','g','f','v','th','s','z','sh','ch','ng','ll']
+cs=['p','b','t','d','c','ck','g','f','v','th','s','z','sh','ch','ng','ll','tt']
 if option == '1':
     for i in range(0, amount):
         string=""
-        for i in range(0, length):
+        for i in range(0, random.randint(int(length[0]), int(length[1]))):
             string=string+random.choice(alphabet)
         print(string)
 elif option == '2':
     for i in range(0, amount):
         string=""
         o=random.randint(0,1)
-
-        while len(string)<length:
+        rand= random.randint(int(length[0]),int(length[1]))
+        while len(string)<rand:
             if o == 0:
                 string=string+random.choice(v)
                 o=1
             elif o == 1:
-                if random.randint(0,2) == 2 and not len(string)+2 > length:
+                if random.randint(0,2) == 2 and not len(string)+2 > rand:
                     string=string+random.choice(cs)
                 else:
                     string=string+random.choice(c)
