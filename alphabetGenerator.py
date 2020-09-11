2e3ea99c1bb145ce2a2a7edf6b955b9c2bf1e66c
TheSavageTeddy <TheSavageTeddy@users.noreply.github.com>
2020-09-11 12:53:17 +0800

    save

--- alphabetGenerator.py
+++ alphabetGenerator.py
@@ -1,2 +1,10 @@
 alphabet=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
+option = input('''
+(1) Print alphabet
+''')
+
+if option == '1':
+    option = input('''
+(1)
+    ''')
